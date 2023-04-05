b403d3b55c36a94994124449b042b9c48d89d0b9
Connor-York <connor.york000@gmail.com>
2023-04-05 16:10:04 +0100

    First Commit

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from ar_track_alvar_msgs.msg import AlvarMarkers
+
+def callback_ar_pose(msg):
+    for marker in msg.markers:
+        rospy.loginfo(marker.id)
+
+
+
+if __name__ == "__main__":
+        rospy.init_node("ar_logger")
+
+        ar_subscriber = rospy.Subscriber( 
+            "ar_pose_marker", AlvarMarkers, callback_ar_pose  
+        )
+        rospy.spin()
+
