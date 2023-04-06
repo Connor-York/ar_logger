10330bc16063c2fa4ca2bd2ade9afa4b614fde72
Connor <connor.york000@gmail.com>
2023-04-06 15:16:19 +0100

    Updated ar_logger.py to store ID's in an array, but only once per ID (hopefully works)

--- src/logger.py
+++ src/logger.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
 import rospy
-
+import csv
 from ar_track_alvar_msgs.msg import AlvarMarkers
+
+#declaring variables, honestly don't know if this is bad python practice
+ID_list = []
+current_marker = 999 #placeholder variable to prevent logging the same ID more than once
 
 def callback_ar_pose(msg):
     for marker in msg.markers:
-        rospy.loginfo(marker.id)
-        rospy.loginfo(marker.pose.pose)
+        # These two just print the ID and Pose to the cmd line
+        #rospy.loginfo(marker.id)
+        #rospy.loginfo(marker.pose.pose)
+        if marker.id != current_marker: # Check to prevent multi-logging
+            current_marker = marker.id
+            ID_list.append(current_marker)
+            rospy.loginfo(current_marker)
+            rospy.loginfo(ID_list)
 
+            
 
 
 if __name__ == "__main__":
