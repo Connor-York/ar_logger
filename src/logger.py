0ef83b59213f5f8bbc1885dc62c6d7f4214ebece
Connor <connor.york000@gmail.com>
2023-04-06 16:25:39 +0100

    Everything works as intended (added comments)!

--- src/logger.py
+++ src/logger.py
@@ -4,12 +4,15 @@ import rospy
 import csv
 from ar_track_alvar_msgs.msg import AlvarMarkers
 import rospkg
-rp = rospkg.RosPack()
-package_path = rp.get_path('ar_logger')
 
-#declaring variables, honestly don't know if this is bad python practice
+
+#declaring global
 ID_list = []
 current_marker = 999 #placeholder variable to prevent logging the same ID more than once
+
+#gets csv file path for saving
+rp = rospkg.RosPack()
+package_path = rp.get_path('ar_logger')
 CSV_path = (package_path + "/logs/IDlog.csv")
 
 def dupe_check(iterable,check):
@@ -33,7 +36,7 @@ def callback_ar_pose(msg):
             rospy.loginfo(current_marker)
             rospy.loginfo(ID_list)
 
-def save_to_csv():
+def save_to_csv(): #called on shutdown, saves csv, overwrites file
     
     with open(CSV_path, 'w') as f:
 
@@ -48,6 +51,8 @@ if __name__ == "__main__":
         ar_subscriber = rospy.Subscriber( 
             "ar_pose_marker", AlvarMarkers, callback_ar_pose  
         )
+
         rospy.spin()
-        rospy.on_shutdown(save_to_csv)
+        rospy.on_shutdown(save_to_csv) #call on shutdown
+
 
